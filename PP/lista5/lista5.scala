6d3f8a96a75f4a0b09842b6a629e6cf037c473b5
Madatt <alter.mail@o2.pl>
2019-11-05 01:30:24 +0100

    ..

---
+++ PP/lista5/lista5.scala
@@ -0,0 +1,60 @@
+sealed trait lBT[+A]
+ case object LEmpty extends lBT[Nothing]
+ case class LNode[+A](elem:A, left:()=>lBT[A], right:()=>lBT[A]) extends lBT[A]
+
+object lista5 {
+  def printList(args: List[_]): Unit = {
+    args.foreach(println)
+  }
+
+  def lrepeat[A] (k: Int)(l: LazyList[A]): LazyList[A] = {
+    def inside (n: Int, ll: LazyList[A]): LazyList[A] = (n, ll) match {
+      case (_, LazyList()) => LazyList()
+      case (0, h#::t) => inside(k, t)
+      case (_, h#::_) => h#::inside(n-1, ll)
+    }
+    inside(k, l)
+  }
+
+
+  def lfib() = {
+    def inside(a: Int, b: Int): LazyList[Int] = {
+      a #:: inside(b, a+b)
+    }
+    inside(0, 1)
+  }
+
+  def lBreadth[A](l: lBT[A]) = {
+    def inside(tl: List[lBT[A]]): LazyList[A] = tl match{
+      case Nil => LazyList()
+      case LEmpty::t => inside(t)
+      case LNode(v, f1, f2)::t => v#::inside(t ++ (List(f1(), f2())))
+    }
+    inside(List(l))
+  }
+
+/*
+let lfib =
+  let rec inside a b =
+    LCons(a, lazy(inside b (a+b) ))
+  in
+    inside 0 1
+;;
+
+  let lBreadth l =
+    let rec inside tl =
+      match tl with
+        | [] -> LNil
+        | [LEmpty] -> LNil
+        | LEmpty::t -> inside t
+        | LNode(v, f1, f2)::t -> LCons(v, lazy(inside (t @ (f1()::f2()::[]))))
+    in
+      inside [l]
+  ;;
+
+*/
+
+  def main(args: Array[String]): Unit = {
+
+  }
+}
