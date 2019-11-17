18d674e5434a14b0e3e6ed8360376c74a390aeb9
Madatt <alter.mail@o2.pl>
2019-11-17 22:06:07 +0100

    ..

---
+++ TEP/lista5/cmake-build-debug/CMakeFiles/lista5.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Madatt/Desktop/Studbaza/TEP/lista5/TreeDynamic.cpp" "C:/Users/Madatt/Desktop/Studbaza/TEP/lista5/cmake-build-debug/CMakeFiles/lista5.dir/TreeDynamic.cpp.obj"
+  "C:/Users/Madatt/Desktop/Studbaza/TEP/lista5/TreeStatic.cpp" "C:/Users/Madatt/Desktop/Studbaza/TEP/lista5/cmake-build-debug/CMakeFiles/lista5.dir/TreeStatic.cpp.obj"
+  "C:/Users/Madatt/Desktop/Studbaza/TEP/lista5/main.cpp" "C:/Users/Madatt/Desktop/Studbaza/TEP/lista5/cmake-build-debug/CMakeFiles/lista5.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
