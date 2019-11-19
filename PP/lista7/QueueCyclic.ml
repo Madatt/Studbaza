3ba0b82edb9f6b03e334a5b28d3e79a9122e8dd8
Madatt <alter.mail@o2.pl>
2019-11-19 22:51:41 +0100

    ..

--- PP/lista7/QueueCyclic.ml
+++ PP/lista7/QueueCyclic.ml
@@ -1,4 +1,28 @@
-open QueueMut
+module type QUEUE_MUT =
+sig
+ type 'a t
+ (* The type of queues containing elements of type ['a]. *)
+ exception Empty of string
+ (* Raised when [first q] is applied to an empty queue [q]. *)
+ exception Full of string
+ (* Raised when [enqueue(x,q)] is applied to a full queue [q]. *)
+ val empty: int -> 'a t
+ (* [empty n] returns a new queue of length [n], initially empty. *)
+ val enqueue: 'a * 'a t -> unit
+ (* [enqueue (x,q)] adds the element [x] at the end of a queue [q]. *)
+ val dequeue: 'a t -> unit
+ (* [dequeue q] removes the first element in queue [q] *)
+ val first: 'a t -> 'a
+ (* [first q] returns the first element in queue [q] without removing
+ it from the queue, or raises [Empty] if the queue is empty. *)
+ val isEmpty: 'a t -> bool
+ (* [isEmpty q] returns [true] if queue [q] is empty,
+ otherwise returns [false]. *)
+ val isFull: 'a t -> bool
+ (* [isFull q] returns [true] if queue [q] is full,
+ otherwise returns [false]. *)
+end;;
+
 
 module QueueCyclic : QUEUE_MUT =
 struct
@@ -27,15 +51,23 @@ struct
 
   let dequeue q =
     if(isEmpty q) then () else
-      q.f <- ((q.r + 1) mod (Array.length q.a));
+      q.f <- ((q.f + 1) mod (Array.length q.a));
   ;;
 
   let first q =
     if(isEmpty q) then raise (Empty "") else
     match q.a.(q.f) with
-      | None -> raise (Empty "") 
+      | None -> raise (Empty "")
       | Some a -> a
     ;;
   ;;
 
 end;;
+
+let q = QueueCyclic.empty 4;;
+QueueCyclic.enqueue(3, q);;
+QueueCyclic.enqueue(4, q);;
+
+print_int (QueueCyclic.first q);;
+QueueCyclic.dequeue q;;
+print_int (QueueCyclic.first q);;
